ff505fccce0639cb72ca6869879f9775d978b4a7
JinxedGrim <88637066+JinxedGrim@users.noreply.github.com>
2023-11-29 21:06:59 -0500

    Update TerraGL.h

--- GraphicsExploration/TerraGL.h
+++ GraphicsExploration/TerraGL.h
@@ -2,30 +2,32 @@
 #include <d2d1.h>
 #include <wincodec.h>
 
+
+#ifdef _MSC_VER
 #pragma comment(lib, "d2d1")
 #pragma comment(lib, "windowscodecs")
-
-// Global Declarations
-ID2D1Factory* pFactory = NULL;
-ID2D1HwndRenderTarget* pRenderTarget = NULL;
-ID2D1Bitmap* pBitmap = NULL;
-ID2D1SolidColorBrush* pBrush = NULL;
+#endif
 
 class TerraGL
 {
     public:
 
     HWND Hwnd = NULL;
-    HDC ScreenDC = NULL;
+
     RECT ClientRect = {};
     D2D1_SIZE_U ScreenSz = {};
 
-    ID2D1HwndRenderTarget* RenderTarget;
     ID2D1Factory* D2D1Factory;
-    D2D1_BITMAP_PROPERTIES BitmapProperties;
 
+    ID2D1HwndRenderTarget* RenderTarget;
     bool CurrentlyDrawing = false;
+
+    D2D1_BITMAP_PROPERTIES BitmapProperties;
+    ID2D1Bitmap* Bitmap;
     BYTE* PixelBuffer = nullptr;
+    int Stride = 0;
+    int PixelBufferWidth = 0;
+    int PixelBufferHeight = 0;
 
     bool UpdateClientRgn(bool UpdatePixelBuffer = false)
     {
@@ -35,14 +37,11 @@ class TerraGL
 
         HRESULT Hr = S_OK;
 
-        Hr = pFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(this->Hwnd, this->ScreenSz), &RenderTarget);
+        Hr = D2D1Factory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(this->Hwnd, this->ScreenSz), &RenderTarget);
 
         if (UpdatePixelBuffer == true)
         {
-            if (PixelBuffer != nullptr)
-                delete[] PixelBuffer;
-
-            PixelBuffer = new BYTE[(ClientRect.right - ClientRect.left) * (ClientRect.bottom - ClientRect.top)];
+            this->SetupPixelBuffer((ClientRect.right - ClientRect.left), (ClientRect.bottom - ClientRect.top), 4);
         }
 
         return Stat == true && SUCCEEDED(Hr);
@@ -53,28 +52,40 @@ class TerraGL
         this->BitmapProperties = D2D1::BitmapProperties(PixelFormat, DpiX, DpiY);
     }
 
+    void SetupPixelBuffer(int Width, int Height, int BytesPerPixel)
+    {
+        this->PixelBufferWidth = Width;
+        this->PixelBufferHeight = Height;
+
+        if (PixelBuffer != nullptr)
+            delete[] PixelBuffer;
+
+        this->Stride = ((this->PixelBufferWidth * BytesPerPixel + 3) / 4) * 4; // Calculate stride
+
+        PixelBuffer = new BYTE[this->Stride * this->PixelBufferHeight];
+    }
+
     TerraGL(HWND _Wnd)
     {
         this->Hwnd = _Wnd;
         this->CurrentlyDrawing = false;
 
         D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &D2D1Factory);
-        this->UpdateClientRgn();
+        this->UpdateClientRgn(true);
 
         this->SetBitmapProperties(D2D1::PixelFormat(DXGI_FORMAT_R8G8B8A8_UNORM, D2D1_ALPHA_MODE_STRAIGHT));
     }
 
-    HRESULT DrawPixelBuffer()
+    bool DrawPixelBuffer()
     {
         HRESULT Hr = S_OK;
 
         if (SUCCEEDED(Hr))
         {
-            Hr = pRenderTarget->CreateBitmap(this->ScreenSz, this->PixelBuffer,
-                bufferWidth * 4, // Assuming 4 bytes per pixel (32 bits)
-                &bitmapProperties,
-                &pBitmap);
+            Hr = this->RenderTarget->CreateBitmap(this->ScreenSz, (void*)this->PixelBuffer, (UINT32)this->Stride, this->BitmapProperties, &Bitmap);
         }
+
+        return SUCCEEDED(Hr);
     }
 
     __inline void BeginDraw()
@@ -99,12 +110,27 @@ class TerraGL
         this->RenderTarget->EndDraw();
         this->CurrentlyDrawing = false;
     }
-};
 
-// Function to release Direct2D resources
-void DiscardGraphicsResources()
-{
-    if (pBitmap) pBitmap->Release();
-    if (pRenderTarget) pRenderTarget->Release();
-    if (pFactory) pFactory->Release();
-}
+    ~TerraGL()
+    {
+        if(this->Bitmap)
+        {
+            Bitmap->Release();
+        }
+
+        if(this->PixelBuffer)
+        {
+            delete[] PixelBuffer;
+        }
+
+        if(this->RenderTarget)
+        {
+            RenderTarget->Release();
+        }
+
+        if(this->D2D1Factory)
+        {
+            D2D1Factory->Release();
+        }
+    }
+};
